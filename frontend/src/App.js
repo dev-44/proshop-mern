9061a9fea4ac5000542a2cd7d17ea78e1168845d
plug47 <oarmoa44@gmail.com>
2022-03-09 09:45:02 -0300

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
+function App() {
+  return (
+    <>
+      <h1>ProShop MERN</h1>
+    </>
+  );
+}
+
+export default App;
