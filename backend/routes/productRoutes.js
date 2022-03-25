7304dcf1080d5e88ad0bfa81f6488b678eb183b7
plug47 <oarmoa44@gmail.com>
2022-03-25 06:14:35 -0300

    creating the backend auth

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,10 @@
 import express from "express"
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
+import {getProducts, getProductById} from '../controllers/productController.js'
 
-//@description      Fetch all products
-//@route            GET api/products
-//@access           Public
-router.get('/', asyncHandler(async (req, res)=> {
-    const products = await Product.find({})
-    res.json(products)
-}))
 
-//@description      Fetch single product
-//@route            GET api/products/:id
-//@access           Public
-router.get('/:id', asyncHandler(async (req, res)=> {
-    //const product = products.find((p) => p._id === req.params.id)
-    const product = await Product.findById(req.params.id)
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
-    if(product){
-        res.json(product)
-    } else {
-        //res.status(404).json({message: 'Product not found'})
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -12,12 +13,16 @@ connectDB()
 
 const app = express()
 
+//Middleware
+app.use(express.json())
+
 app.get('/', (req, res)=> {
     res.send('API is running')
 })
 
 //Routing
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //Error Handler
 app.use(notFound)
