c7dea67c8463dbc28495b68e2ca6b551ba00b125
plug47 <oarmoa44@gmail.com>
2022-03-26 14:10:16 -0300

    register and auth user in the backend done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,63 @@ const authUser = asyncHandler(async(req, res) => {
     }
 })
 
+//@description      Register a new user
+//@route            POST api/users
+//@access           Public
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already Exists')
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalidad User Data')
+
+    }
+})
+
+//@description      Get user profile
+//@route            POST api/users/profile
+//@access           Private
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 //Generate Token
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
@@ -32,4 +89,4 @@ const generateToken = (id) => {
     })
 }
 
-export {authUser}
+export {authUser, getUserProfile, registerUser}
