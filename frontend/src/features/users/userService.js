8eebd4d44bae1bdd92d9aa11309ba90d79f585e4
plug47 <oarmoa44@gmail.com>
2022-03-29 06:19:02 -0400

    implementing update pf

--- frontend/src/features/users/userService.js
+++ frontend/src/features/users/userService.js
@@ -47,6 +47,25 @@ const logout = () => {
     localStorage.removeItem('user')
 }
 
+//Update User
+export const updateUser = async(userData) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${userData.token}`
+        }
+    }
+
+    const {data} = await axios.get(API_URL, userData, config)
+    console.log(data)
+
+    if(data){
+        localStorage.setItem('user', JSON.stringify(data))
+    }
+
+    return data
+}
+
 const userService = {
     register,
     login,
