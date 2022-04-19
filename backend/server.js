f8fb43fafa30878fc5c6c109c3025a591d757470
plug47 <oarmoa44@gmail.com>
2022-04-19 09:39:58 -0400

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ if(process.env.NODE_ENV ==='development') {
 app.use(express.json({limit: '16mb', extended: true}))
 app.use(express.urlencoded({limit: '16mb', extended: true}))
 
-app.get('/', (req, res)=> {
-    res.send('API is running')
-})
-
 //Routing
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -40,6 +36,16 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 //Upload Images
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+//Deployment
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))    //All routes that have not been declared              
+} else {
+    app.get('/', (req, res)=> {
+        res.send('API is running')
+    })
+}
 
 //Error Handler
 app.use(notFound)
